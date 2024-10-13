8f03abb76e6e0a1c64ec5b0fcaa897a9301ffce4
Sadiq Quliyev <sadiq000k@gmail.com>
2024-10-13 18:47:50 +0400

    Proje dosyası ekle.

---
+++ ConsoleApplication31/ConsoleApplication31.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+//int main()
+//{
+//	cout << "Eded daxil edin: ";
+//	int num;
+//	cin >> num;
+//	cout << "Sola surusmeni daxil edin: ";
+//	int s;
+//	cin >> s;
+//	int uzun = 0;
+//	int i = num;
+//	do
+//	{
+//		uzun++;
+//		i /= 10;
+//	} while (i > 0);
+//	int u = 1;
+//	i = uzun;
+//	do
+//	{
+//		u *= 10;
+//		i--;
+//
+//	} while (i > 1);
+//
+//	do
+//	{
+//		int q = num / u;
+//		num-= q * u;
+//		num*= 10;
+//		num+= q;
+//		 s++;
+//	} while (s > 0);
+//	cout << num << endl;
+//
+//	
